112f5f5d6918c9ea8d56855425d180e8a3865470
BlahBlah <leesemin222@gmail.com>
2023-07-11 15:39:14 +0900

    commander 설정하기 Fixes dltpals222/forkAllIssue#2

--- forkAllIssue.ts
+++ forkAllIssue.ts
@@ -1,6 +1,8 @@
 import inquirer from "inquirer";
 import { program } from "commander";
 import { execSync } from "child_process";
+import fs from "fs";
+import path from "path";
 
 program
   .command("repo")
@@ -28,10 +30,29 @@ program
     const { address } = await inquirer.prompt(questionsGitAddress);
     const addressSplit = address.split("/")[4];
     const dirAddress = addressSplit.split(".git")[0];
-    execSync(`git clone ${address}`);
-    execSync(`ac .gitignore ${dirAddress}`);
-    execSync(`git fetch upstream`, { cwd: `${dirAddress}` });
-    execSync(`git push origin refs/remotes/upstream/*:refs/heads/*`, { cwd: `${dirAddress}` });
+    const dirExists = fs.existsSync(`./${dirAddress}`);
+    const dirPath = path.join(path.resolve() + dirAddress);
+    if (dirExists) {
+      console.log("파일이 존재합니다.");
+    } else {
+      console.log("깃 클론 중");
+      execSync(`cd ../ && git clone ${address} && cd ${dirAddress}`);
+    }
+    try {
+      execSync(`git remote add upstream ${address}`);
+    } catch (error) {
+      console.log("git remote add upstream 에러", error);
+    }
+    try {
+      execSync(`git fetch upstream`);
+    } catch (error) {
+      console.log("git fetch upstream 에러", error);
+    }
+    try {
+      execSync(`git push origin refs/remotes/upstream/*:refs/heads/*`);
+    } catch (error) {
+      console.log("git push origin refs/remotes/upstream/*:refs/heads/* 에러", error);
+    }
   });
 
 program.parse(process.argv);
