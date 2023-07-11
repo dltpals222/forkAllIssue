c084484504b78c7100515f6c6361dfb27e866db6
BlahBlah <leesemin222@gmail.com>
2023-07-11 12:48:52 +0900

    inquirer 끝

---
+++ test.js
@@ -0,0 +1,6 @@
+const a = "https://github.com/SBoudrias/Inquirer.js.git";
+const a1 = "https://github.com/kongukjae/KDT-2-Project-A-4.git";
+
+const b = a.split("/");
+
+console.log(b[4].split(".git"));
