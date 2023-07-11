1a49fbac4cf426d96239b7afee5b978657da89f0
BlahBlah <leesemin222@gmail.com>
2023-07-11 14:29:37 +0900

    script추가 및 테스트 중

--- forkAllIssue.ts
+++ forkAllIssue.ts
@@ -5,6 +5,7 @@ import { execSync } from "child_process";
 program
   .command("repo")
   .description("이슈를 업데이트할 깃 주소를 입력하세요.")
+  .option("-A,--address [address]", "깃 주소를 입력하세요")
   .action(async () => {
     const questionsGitAddress = [
       {
@@ -16,7 +17,7 @@ program
             return "깃 주소를 입력해주세요.";
           }
           if (value.endsWith(".git")) {
-            return value;
+            return true;
           } else {
             return "깃 주소를 정확히 입력해주세요.";
           }
@@ -28,10 +29,9 @@ program
     const addressSplit = address.split("/")[4];
     const dirAddress = addressSplit.split(".git")[0];
     execSync(`git clone ${address}`);
-    execSync(`sc .gitignore ${dirAddress}`);
-    execSync(`cd ${dirAddress}`);
-    execSync(`git fetch upstream`);
-    execSync(`git push origin refs/remotes/upstream/*:refs/heads/*`);
+    execSync(`ac .gitignore ${dirAddress}`);
+    execSync(`git fetch upstream`, { cwd: `${dirAddress}` });
+    execSync(`git push origin refs/remotes/upstream/*:refs/heads/*`, { cwd: `${dirAddress}` });
   });
 
 program.parse(process.argv);
