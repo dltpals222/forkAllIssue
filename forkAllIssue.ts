9f7e8016b5bbf249c3f7cfb46a3801034a97551b
BlahBlah <leesemin222@gmail.com>
2023-07-11 12:49:49 +0900

    실행 에러

--- forkAllIssue.ts
+++ forkAllIssue.ts
@@ -1,46 +1,37 @@
 import inquirer from "inquirer";
+import { program } from "commander";
+import { execSync } from "child_process";
 
-// const questionsNewDirectory = [
-//   {
-//     type: "confirm",
-//     name: "newDirectory",
-//     message: "디렉토리를 생성하시겠습니까?",
-//     default: false,
-//   },
-// ];
+program
+  .command("repo")
+  .description("이슈를 업데이트할 깃 주소를 입력하세요.")
+  .action(async () => {
+    const questionsGitAddress = [
+      {
+        type: "input",
+        name: "address",
+        message: "깃 주소를 입력하세요",
+        validate: function (value: string) {
+          if (!value) {
+            return "깃 주소를 입력해주세요.";
+          }
+          if (value.endsWith(".git")) {
+            return value;
+          } else {
+            return "깃 주소를 정확히 입력해주세요.";
+          }
+        },
+      },
+    ];
 
-// const questionsMkdir = [
-//   {
-//     type: "input",
-//     name: "mkdir",
-//     message: "디렉토리명을 입력하세요.",
-//   },
-// ];
-
-const questionsGitAddress = [
-  {
-    type: "input",
-    name: "address",
-    message: "깃 주소를 입력하세요",
-    validate: function (value: string) {
-      if (!value) {
-        return "깃 주소를 입력해주세요.";
-      }
-      if (value.endsWith(".git")) {
-        return value;
-      } else {
-        return "깃 주소를 정확히 입력해주세요.";
-      }
-    },
-  },
-];
-
-// inquirer.prompt(questionsGitAddress).then((answers: string) => {
-//   console.log("일단 보자", answers);
-// });
-
-(async () => {
-  inquirer.prompt(questionsGitAddress).then((answers: string) => {
-    console.log("일단 보자", answers);
+    const { address } = await inquirer.prompt(questionsGitAddress);
+    const addressSplit = address.split("/")[4];
+    const dirAddress = addressSplit.split(".git")[0];
+    execSync(`git clone ${address}`);
+    execSync(`sc .gitignore ${dirAddress}`);
+    execSync(`cd ${dirAddress}`);
+    execSync(`git fetch upstream`);
+    execSync(`git push origin refs/remotes/upstream/*:refs/heads/*`);
   });
-})();
+
+program.parse(process.argv);
